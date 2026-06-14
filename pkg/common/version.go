760c15f81cf0146534f9b0651a3c9cbcd8e7b7ee
anh-chu <chuducanh.atred@gmail.com>
2026-06-14 11:03:45 +0000

    chore(release): 0.7.4

--- pkg/common/version.go
+++ pkg/common/version.go
@@ -4,13 +4,13 @@ package common
 var NAME = "guppi"
 
 // SUMMARY of the Version
-var SUMMARY = "v0.7.3" // x-release-please-version
+var SUMMARY = "v0.7.4" // x-release-please-version
 
 // BRANCH of the Version
 var BRANCH = "dev"
 
 // VERSION of Release
-var VERSION = "0.7.3" // x-release-please-version
+var VERSION = "0.7.4" // x-release-please-version
 
 var COMMIT = "dirty"
 
