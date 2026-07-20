cd9ca1d6998fd37abc1789f3aa6005e8d6646a38
anh-chu <chuducanh.atred@gmail.com>
2026-07-20 16:11:55 +0700

    chore(release): 4.0.4

--- pkg/common/version.go
+++ pkg/common/version.go
@@ -4,13 +4,13 @@ package common
 var NAME = "termyard"
 
 // SUMMARY of the Version
-var SUMMARY = "v4.0.3" // x-release-please-version
+var SUMMARY = "v4.0.4" // x-release-please-version
 
 // BRANCH of the Version
 var BRANCH = "dev"
 
 // VERSION of Release
-var VERSION = "4.0.3" // x-release-please-version
+var VERSION = "4.0.4" // x-release-please-version
 
 var COMMIT = "dirty"
 
